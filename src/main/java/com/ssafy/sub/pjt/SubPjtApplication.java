4458d3be428e63a9a9a898071e87f4f9d040f028
minjeong <a50070863@gmail.com>
2024-01-08 17:24:06 +0900

    refactor: spotless 적용 (#4)

--- src/main/java/com/ssafy/sub/pjt/SubPjtApplication.java
+++ src/main/java/com/ssafy/sub/pjt/SubPjtApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SubPjtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SubPjtApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SubPjtApplication.class, args);
+    }
 }
