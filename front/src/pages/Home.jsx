c945f421ee29206edf7e22c57624f814439498d1
wjdxodbs <wjdxodbs52@naver.com>
2024-01-15 13:50:00 +0900

    setting: front 폴더 생성

---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+const Home = () => {
+  // useEffect(() => {}, []);
+
+  return (
+    <div>
+      <div>메인페이지</div>
+    </div>
+  );
+};
+
+export default Home;
