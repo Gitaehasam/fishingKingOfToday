764ce9210710f5db6177437100f330c01b73cf5a
강민정 <a50070863@gmail.com>
2024-01-20 18:45:38 +0900

    [Feature] 라이브 방송 도메인 추가

---
+++ back/src/main/java/com/ssafy/sub/pjt/domain/LiveRoom.java
@@ -0,0 +1,30 @@
+package com.ssafy.sub.pjt.domain;
+
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LiveRoom extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private int id;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @ManyToOne
+    @Column(name = "owner_id")
+    private Integer ownerId;
+}
