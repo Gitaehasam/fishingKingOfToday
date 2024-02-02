850b79f972ae7f719f50c278bacf5411a4260a41
정태윤 <wjdxodbs52@naver.com>
2024-02-03 00:05:46 +0900

    [Setting] 로컬, 개발, 배포 환경에 따른 환경변수 적용

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
