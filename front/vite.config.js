b670005cae2542cbd19145cf2e54b050cfefeadd
정태윤 <wjdxodbs52@naver.com>
2024-02-06 16:09:39 +0900

    [Feature] PWA 설정

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,9 +2,66 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import dotenv from "dotenv";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "오늘은 낚시왕",
+        short_name: "오늘은 낚시왕",
+        start_url: "/",
+        icons: [
+          {
+            src: "./icons/apple-touch-icon-57x57.png",
+            sizes: "57x57",
+            type: "image/png",
+          },
+          {
+            src: "./icons/apple-touch-icon-60x60.png",
+            sizes: "60x60",
+            type: "image/png",
+          },
+          {
+            src: "./icons/apple-touch-icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "./icons/apple-touch-icon-76x76.png",
+            sizes: "76x76",
+            type: "image/png",
+          },
+          {
+            src: "./icons/apple-touch-icon-114x114.png",
+            sizes: "114x114",
+            type: "image/png",
+          },
+          {
+            src: "./icons/apple-touch-icon-120x120.png",
+            sizes: "120x120",
+            type: "image/png",
+          },
+
+          {
+            src: "./icons/apple-touch-icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "./icons/apple-touch-icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 5173,
     host: "0.0.0.0",
