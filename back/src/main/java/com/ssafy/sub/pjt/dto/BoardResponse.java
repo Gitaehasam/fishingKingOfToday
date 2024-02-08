da7d3ab84c83449b77a4c3347c69f0dc27761734
강민정 <a50070863@gmail.com>
2024-02-08 18:11:13 +0900

    [Feature] 잡은 물고기 게시판 응답값 수정

--- back/src/main/java/com/ssafy/sub/pjt/dto/BoardResponse.java
+++ back/src/main/java/com/ssafy/sub/pjt/dto/BoardResponse.java
@@ -5,6 +5,8 @@ import static lombok.AccessLevel.PRIVATE;
 import com.ssafy.sub.pjt.domain.BoardData;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -16,7 +18,9 @@ public class BoardResponse {
     private final String profileImageUrl;
     private final Integer boardId;
     private final String boardImageUrl;
+    private final String fishName;
     private final List<String> hashtags;
+    private final String fishName;
     private final String content;
     private final LocalDateTime createdAt;
     private final Integer commentCnt;
@@ -29,7 +33,9 @@ public class BoardResponse {
                 boardData.getProfileImageUrl(),
                 boardData.getBoardId(),
                 boardData.getBoardImageUrl().getUrl(),
+                boardData.getFishName(),
                 boardData.getHashtags(),
+                boardData.getFishName(),
                 boardData.getContent(),
                 boardData.getCreatedAt(),
                 commentCnt,
