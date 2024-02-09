79697ecc5cfd30e2e77849b96d792614787bb7d3
허승경 <hhe98hsk@gmail.com>
2024-02-09 13:11:56 +0900

    [Feature] 어류 도감 상세 조회 요청 DTO Validation

--- back/src/main/java/com/ssafy/sub/pjt/dto/FishBookDetailRequest.java
+++ back/src/main/java/com/ssafy/sub/pjt/dto/FishBookDetailRequest.java
@@ -1,10 +1,13 @@
 package com.ssafy.sub.pjt.dto;
 
+import javax.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class FishBookDetailRequest {
+
+    @Positive(message = "물고기 ID는 양수만 가능합니다.")
     private Integer fishBookId;
 }
